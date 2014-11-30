a72be03d137f12ebf2721e1af406818639c22551
Andrew Head <head.andrewm@gmail.com>
2014-11-30 13:57:44 -0800

    Launch: Actually perform last commit

---
+++ observer/network_reboot.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+SERVER_URL = "www.gallerypaths.com"
+LOG_FILE = /var/log/gallery/network
+
+curl $SERVER_URL
+if [ $? -ne 0 ]
+then
+  echo "[`date`] Internet down.  Rebooting..." >> $LOG_FILE
+  ifdown wlan0; sleep 5s; ifup wlan0;
+else
+  echo "[`date`] Internet still up!" >> $LOG_FILE
+fi
