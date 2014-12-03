cf9976e5f450912803f7e1026214d75ffb33384a
Andrew Head <head.andrewm@gmail.com>
2014-12-03 02:41:21 -0800

    UI: put space between lines in chord diagram.

--- server/gallery/static/gallery/js/analytics.js
+++ server/gallery/static/gallery/js/analytics.js
@@ -4,17 +4,6 @@ $(function(){
         return Number(str.replace("px", ""));
     };
 
-    /* Dwell time chart */
-    /*
-    var dwellTimeData = {
-        0: 200,
-        1: 175,
-        2: 250,
-        3: 275,
-        4: 5,
-        5: 120
-    }
-    */
     var margin = {top: 10, bottom: 10, right: 10, left: 60};
     var chart = d3.select("#dwell_chart");
     var w = px_to_num(chart.style("width"));
@@ -26,6 +15,9 @@ $(function(){
     var y_scale = d3.scale.ordinal()
         .domain(d3.keys(dwellTimeData))
         .rangeRoundBands([0, h - margin.bottom], .1);
+    var color_scale = d3.scale.linear()
+        .domain([0, d3.max(d3.values(dwellTimeData))])
+        .range(["#000000", "#008800"]);
 
     var svg = chart.append("svg")
         .style("width", w)
@@ -40,7 +32,7 @@ $(function(){
     var rects = bars
         .append("rect")
         .attr("class", "time_bar")
-        .style("fill", "green")
+        .style("fill", function(d) { return color_scale(d.value); })
         .attr("x", margin.left)
         .attr("width", function(d) { return w_scale(d.value); })
         .attr("height", y_scale.rangeBand());
@@ -53,24 +45,6 @@ $(function(){
         .attr("transform", "translate(" + (margin.left - 20) + "," + y_scale.rangeBand() / 1 + ")");
 
     /*  Chord chart */
-    /* We assume that visit data is already sorted when it comes back from the server */
-    /*
-    var sightings = [
-        {user: 1, tripIndex: 1, time: new Date("2014-01-01T12:00:00Z"), location: 1},
-        {user: 1, tripIndex: 1, time: new Date("2014-01-01T12:01:00Z"), location: 2},
-        {user: 1, tripIndex: 1, time: new Date("2014-01-01T12:02:00Z"), location: 3},
-        {user: 1, tripIndex: 1, time: new Date("2014-01-01T12:03:00Z"), location: 4},
-        {user: 1, tripIndex: 1, time: new Date("2014-01-01T12:04:00Z"), location: 5},
-        {user: 1, tripIndex: 1, time: new Date("2014-01-01T12:05:00Z"), location: 6},
-        {user: 2, tripIndex: 1, time: new Date("2014-01-01T13:00:00Z"), location: 1},
-        {user: 2, tripIndex: 1, time: new Date("2014-01-01T13:01:00Z"), location: 3},
-        {user: 2, tripIndex: 1, time: new Date("2014-01-01T13:02:00Z"), location: 5},
-        {user: 2, tripIndex: 1, time: new Date("2014-01-01T13:03:00Z"), location: 6},
-        {user: 2, tripIndex: 1, time: new Date("2014-01-01T13:04:00Z"), location: 4},
-        {user: 2, tripIndex: 1, time: new Date("2014-01-01T13:05:00Z"), location: 2}
-    ];
-    */
-
     /* Nest data by user and trip index. */
     var paths = d3.nest()
         .key(function(d) { return d.user; })
@@ -132,9 +106,12 @@ $(function(){
 
     var bundle = d3.layout.bundle();
 
+    var tension_scale = d3.scale.linear()
+        .domain([0, links.length])
+        .range([0, 1]);
+
     var line = d3.svg.line.radial()
         .interpolate("bundle")
-        .tension(.4)
         .radius(function(d) { return d.y; })
         .angle(function(d) { return d.x / 180 * Math.PI; });
 
@@ -150,7 +127,7 @@ $(function(){
           .data(bundle(links))
         .enter().append("path")
           .attr("class", "link")
-          .attr("d", line);
+          .attr("d", function(d, i) { return line.tension(tension_scale(i))(d); });
 
       svg.selectAll(".node")
           .data(nodes.filter(function(n) { return !n.children; }))
--- server/gallery/static/gallery/js/paths.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    console.log("Hello paths!");
-    $("#time_range").slider();
-    $("#time_range").on("slide", function() {
-        console.log("Range changed");
-    });
-});
