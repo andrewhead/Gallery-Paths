dfebbf4f0b896991791e44ab42a40f2b3175a3d1
Andrew Head <head.andrewm@gmail.com>
2014-12-03 01:11:33 -0800

    Server: Enable logging

--- server/gallery/settings/production.py
+++ server/gallery/settings/production.py
@@ -12,3 +12,32 @@ ALLOWED_HOSTS = [
     '.gallerypaths.com',
     '.gallerypaths.com.',
 ]
+
+LOGGING = {
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S",
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/gallery.log',
+            'formatter': 'verbose',
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'propagate': True,
+            'level': 'DEBUG',
+        }
+        'gallery': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+         },
+    }
+}
